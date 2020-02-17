16abcd38775361649b34f9590f45c8f2f02b6dfb
Mohamed Amr <mohamed,amr@avelabs.com>
2020-02-17 21:42:03 +0200

    re Designed

--- main.c
+++ main.c
@@ -7,13 +7,8 @@
 
 #include <stdio.h>
 
-/********************************************************************************************
- *									Function Prototypes
- *********************************************************************************************/
-static void admin_Mode (void) ;
-static void user_Mode  (void) ;
 
-
+#define DATA_BASE_LIMIT        255
 
 /********************************************************************************************
  *									Patient Info
@@ -23,13 +18,25 @@ static void user_Mode  (void) ;
 typedef struct
 {
 
-	unsigned char patientName[10]     ;
-	unsigned char patientGender[10]   ;
-	unsigned char patientAge		  ;
-	unsigned char patientID			  ; /* if ID Exist -> retry */
+	unsigned char patientName[8]     ;
+	unsigned char patientGender[8]   ;
+	unsigned char patientAge		 ;
+	unsigned char patientID			 ; /* if ID Exist -> retry */
 
 }Patient;
 
+
+
+
+Patient dataBase[DATA_BASE_LIMIT] ;
+
+unsigned char dataBaseIndex = 0 ; ;
+
+void init_System (void)
+{
+	dataBaseIndex = 0 ;
+	dataBase[dataBaseIndex].patientID = 1 ;
+}
 
 
 typedef enum
@@ -49,6 +56,61 @@ typedef enum
 
 
 /********************************************************************************************
+ *									Function Prototypes
+ *********************************************************************************************/
+static void admin_Mode (void) ;
+static void user_Mode  (void) ;
+
+
+
+
+
+static void add_Patient (void)
+{
+
+	printf("Enter Patient Name\n");
+	fflush(stdout);
+
+	scanf("%[^\n]",dataBase[dataBaseIndex].patientName);
+	fflush(stdin);
+
+	printf("Enter Patient Gender\n");
+	fflush(stdout);
+
+	scanf("%[^\n]",dataBase[dataBaseIndex].patientGender);
+	fflush(stdin);
+
+	printf("Enter Patient Age\n");
+	fflush(stdout);
+
+	scanf("%c",  &(dataBase[dataBaseIndex].patientAge));
+	fflush(stdin);
+
+	printf("Enter Patient ID\n");
+	fflush(stdout);
+
+	scanf("%c", &(dataBase[dataBaseIndex].patientID));
+	fflush(stdin);
+
+	dataBaseIndex++;
+
+}
+
+
+
+static void printPatientInfo (Patient* string)
+{
+
+	printf("%s\t", string->patientName)   ;
+	printf("%s\t", string->patientGender) ;
+	printf("%d\t", string->patientAge)    ;
+	printf("%d\t", string->patientID)     ;
+
+}
+
+
+
+/********************************************************************************************
  *										Main Routine
  *********************************************************************************************/
 
@@ -56,10 +118,9 @@ typedef enum
 int main (void)
 {
 
+	init_System();
 
-
-
-
+	printf("%c",dataBase[0].patientName[7]);
 	return 0 ;
 }
 
@@ -85,9 +146,6 @@ void admin_Mode (void)
  * 1 -
  *
  */
-
-
-/*TODO: */
 
 
 
